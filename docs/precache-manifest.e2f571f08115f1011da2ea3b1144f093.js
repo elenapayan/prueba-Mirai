3ad2448d5538137ba39393e767edf89541172c63
elenapayan <elena.payan.glez@gmail.com>
2020-02-20 17:47:32 +0100

    push docs

---
+++ docs/precache-manifest.e2f571f08115f1011da2ea3b1144f093.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e63461f4d4b3d33cbb4ea9eeee3b2d80",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1732ab0632b7c35ae37a",
+    "url": "/static/css/main.38c59c76.chunk.css"
+  },
+  {
+    "revision": "9633870eb3f685579fd1",
+    "url": "/static/js/2.79f8d7f6.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.79f8d7f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1732ab0632b7c35ae37a",
+    "url": "/static/js/main.5a61f0ba.chunk.js"
+  },
+  {
+    "revision": "b6b18212d74d85b85567",
+    "url": "/static/js/runtime-main.91bd1a97.js"
+  }
+]);
