67dcd55a302e9ec26c25d6551064b28a133c3c9a
elenapayan <elena.payan.glez@gmail.com>
2020-02-20 17:46:09 +0100

    Add link to return after search

--- src/components/RoomsDetails.js
+++ src/components/RoomsDetails.js
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 
 function RoomsDetails(props) {
   return (
     <li className="list-group-item">
+      <Link to="/"><span>&lt;</span> Volver</Link>
       <h3 className="list-group-item">Room: {props.roomName}</h3>
       <p className="list-group-item">Offer: {props.offerName}</p>
       <p className="list-group-item">Board: {props.boardName}</p>
